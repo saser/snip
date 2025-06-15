3c218cde733a4475b38aeceb7f9a3540a28d5270
Christian Persson <582273+saser@users.noreply.github.com>
2025-06-15 13:50:04 +0100

    main: optionally include header with current date and timezone.
    The header is included by default; use `-include_header=false` to turn it off.
    Also change default time format to just be the 24h clock, without date and
    timezone.
    
    The reason for this change is that when I'm `cat`ing together a bunch of snip
    files, it becomes pretty difficult to overview. The timestamps are very detailed
    and noisy, and it's difficult to glance where a day starts and ends. Including a
    header at the top of the file, and making the timestamp format relative to that
    header, makes it much easier to overview.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/saser/snip
 
 go 1.23.2
+
+require github.com/google/renameio/v2 v2.0.0
--- main.go
+++ main.go
@@ -11,15 +11,17 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
+
+	"github.com/google/renameio/v2"
 )
 
-const snippetTimeFormat = time.DateTime + " -0700"
-
 var (
-	message = flag.String("m", "", "Title of the snippet. If this is empty then $EDITOR will open to write the snippet, ignoring the -edit flag.")
-	edit    = flag.Bool("edit", false, "Open $EDITOR to edit the snippet. Only has effect if -m is specified. If $EDITOR is empty then vim will be used; if vim is not present on the system, an error is returned.")
-	format  = flag.String("format", snippetTimeFormat, "Format of pre-filled timestamp in snippet. Please refer to https://pkg.go.dev/time to read about time formats.")
+	message       = flag.String("m", "", "Title of the snippet. If this is empty then $EDITOR will open to write the snippet, ignoring the -edit flag.")
+	edit          = flag.Bool("edit", false, "Open $EDITOR to edit the snippet. Only has effect if -m is specified. If $EDITOR is empty then vim will be used; if vim is not present on the system, an error is returned.")
+	format        = flag.String("format", "15:04", "Format of pre-filled timestamp in snippet. Please refer to https://pkg.go.dev/time to read about time formats.")
+	includeHeader = flag.Bool("include_header", true, "Include a header containing the current date and timezone as the first line in the snippet file.")
 )
 
 // baseDir returns the base directory for everything related to snip (snippets
@@ -108,6 +110,22 @@ func run() error {
 	snippet = append(snippet, '\n')
 	// TODO: add future processing, such as validation, here.
 
+	// Assemble the final snippet file and write it out to disk, creating any
+	// directories required. To prevent 0-byte or half-written snippet files,
+	// write out the result to a temporary file and then atomically move it into
+	// place using the github.com/google/renamio package. This might seem
+	// excessive for something that's just personal notes stored locally, but
+	// for the author of this program these snippets are very valuable, so it's
+	// worth being a bit paranoid.
+	//
+	// The final snippet file should include:
+	// * The header, if -include_header=true.
+	//     * If the snippet file already includes a header, but
+	//       -include_header=false in this invocation, leave the header there
+	//       i.e. don't remove it.
+	// * Any existing snippet lines.
+	// * The new snippet line.
+
 	// Write the snippet out to its file, potentially creating all necessary
 	// directories in its path first. If the file already exists, the snippet
 	// will be added at the bottom.
@@ -118,19 +136,100 @@ func run() error {
 	if err := os.MkdirAll(filepath.Dir(path), fs.FileMode(0o755)); err != nil {
 		return fmt.Errorf("write snippet out to file: ensure directory exists: %v", err)
 	}
-	snippetFile, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0o600)
-	if err != nil {
-		return fmt.Errorf("write snippet out to file: open or create snippet file: %v", err)
+
+	// If the snippet file already exists, read it back in. We might need to add
+	// the header, and we need to include any existing snippet lines.
+	existing, err := os.ReadFile(path)
+	if errors.Is(err, os.ErrNotExist) {
+		// The file doesn't exist, which is fine, just initialize with empty
+		// contents.
+		existing = nil
+	} else if err != nil {
+		// Some other error occurred and we don't know how to handle it.
+		return fmt.Errorf("write snippet out to file: read existing snippets: %v", err)
 	}
-	defer func() {
-		if err := snippetFile.Close(); err != nil {
-			log.Printf("Closing snippet file failed unexpectedly: %v", err)
+	var assembled bytes.Buffer
+
+	// The only time we need to format the header and write it out is if
+	// -include_header=true and the file doesn't already contain a header. In
+	// all other cases, we don't need to do anything:
+	// * -include_header=true  && contains header        => do nothing
+	// * -include_header=false && contains header        => do nothing
+	// * -include_header=false && doesn't contain header => do nothing
+	// We won't try to parse the header into a date, as that is too fragile.
+	// Instead we simply look for whether the file starts with "---", which we
+	// use as a proxy for "does the file contain the header".
+	if *includeHeader && !bytes.HasPrefix(existing, []byte("---")) {
+		timezone, err := inferLocalTimezone()
+		if err != nil {
+			log.Printf("Failed to infer local timezone: %v", err)
+			timezone = "<unknown timezone>"
 		}
-	}()
-	if _, err := snippetFile.Write(snippet); err != nil {
+		headerFormat := "--- Monday Jan _2 2006 in " + timezone + " ---"
+		assembled.WriteString(now.Format(headerFormat) + "\n")
+	}
+
+	// Include the existing snippets, if any.
+	assembled.Write(existing)
+	// In case the existing snippets didn't contain a newline, write one out, so
+	// that the new snippet is guaranteed to be on a new line.
+	if existing[len(existing)-1] != '\n' {
+		assembled.WriteByte('\n')
+	}
+
+	// Finally, add the new snippet at the end. Note that we explicitly
+	// construct it to hold a newline above, so we don't need to check for/add
+	// it here.
+	assembled.Write(snippet)
+
+	// Atomically write out the assembled contents to the snippet file.
+	if err := renameio.WriteFile(path, assembled.Bytes(), fs.FileMode(0o600)); err != nil {
 		return fmt.Errorf("write snippet out to file: %v", err)
 	}
 	return nil
+}
+
+// inferLocalTimezone attempts to figure out the IANA name of the local timezone
+// (e.g. "Europe/Stockholm" or "America/Los_Angeles"). It's done on best effort
+// basis, since macOS doesn't provide any explicit way to query for it.
+//
+// This function uses the value of the TZ environment variable, if set, as long
+// as it is a valid location according to [time.LoadLocation].
+func inferLocalTimezone() (string, error) {
+	// Let the TZ environment variable take precedence, if it's set and resolves
+	// to a valid timezone using [time.LoadLocation].
+	if tz := os.Getenv("TZ"); tz != "" {
+		if _, err := time.LoadLocation(tz); err == nil { // if NO error
+			return tz, nil
+		}
+	}
+	// Best-effort: assume that /etc/localtime is a symlink to a file whose path
+	// contains the timezone name in a standardized format. On my macOS system,
+	// it looks like this:
+	//
+	//     $ readlink /etc/localtime
+	//     /var/db/timezone/zoneinfo/Europe/London
+	//
+	// To be a bit more liberal in the paths accepted, look for a "zoneinfo/"
+	// substring, and assume everything after it is the timezone name.
+	//
+	// As a sanity check, try loading the inferred timezone with
+	// [time.LoadLocation]. If that doesn't work, return an error.
+	const localtime = "/etc/localtime"
+	realPath, err := filepath.EvalSymlinks(localtime)
+	if err != nil {
+		return "", fmt.Errorf("infer local timezone: evaluate %s as a symlink: %w", localtime, err)
+	}
+	const marker = "zoneinfo/"
+	idx := strings.Index(realPath, marker)
+	if idx == -1 {
+		return "", fmt.Errorf("infer local timezone: infer from %s symlink: real path does not contain %q", localtime, marker)
+	}
+	inferred := realPath[idx+len(marker):]
+	if _, err := time.LoadLocation(inferred); err != nil {
+		return "", fmt.Errorf("infer local timezone: infer from %s symlink: inferred timezone %q cannot be loaded with time.LoadLocation: %w", localtime, inferred, err)
+	}
+	return inferred, nil
 }
 
 func main() {
